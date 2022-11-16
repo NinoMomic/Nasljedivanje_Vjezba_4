491b32b3ec147cb943e4f58ab0d3e277bb6ba880
NinoMomic <58861649+NinoMomic@users.noreply.github.com>
2022-11-16 08:54:58 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nasljedjivanje_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Bjelogoricno Hrast = new Bjelogoricno(false);
+            Crnogoricno Bor = new Crnogoricno(true);
+            Console.WriteLine("Hrast: " + Hrast.ToString());
+            Console.WriteLine("Bor: "+Bor.ToString());
+
+            Console.ReadKey();
+        }
+    }
+    class Biljka
+    {
+
+    }
+
+    class Stablo : Biljka
+    {
+        bool otpadajuListovi;
+        public bool OtpadajuListovi { get => otpadajuListovi; set => otpadajuListovi = value; }
+        public Stablo(bool otpadajuListovi)
+        {
+            OtpadajuListovi = otpadajuListovi;
+        }
+    }
+
+    class Cvijet : Biljka
+    {
+
+    }
+
+    class Crnogoricno : Stablo
+    {
+        bool otpadajulistovi;
+        public Crnogoricno(bool OtpadajuListovi) : base(OtpadajuListovi)
+        {
+            otpadajulistovi = OtpadajuListovi;   
+        }
+        public override string ToString()
+        {
+            return "OtpadajuListovi = " + otpadajulistovi;
+        }
+    }
+
+    class Bjelogoricno : Stablo
+    {
+        bool _OtpadajuListovi;
+        public Bjelogoricno(bool OtpadajuListovi) : base(OtpadajuListovi)
+        {
+            _OtpadajuListovi= OtpadajuListovi;  
+        }
+        public override string ToString()
+        {
+            return "OtpadajuListovi = " + _OtpadajuListovi;
+        }
+    }
+}
